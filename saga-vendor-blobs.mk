814bcab51ff359484503b7ba1bfb95787029b462
kylon <kylonhck@gmail.com>
2013-11-15 20:21:41 +0100

    Move out common proprietaries(NeverGone-RU)

--- saga-vendor-blobs.mk
+++ saga-vendor-blobs.mk
@@ -16,18 +16,12 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
-    vendor/htc/saga/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
     vendor/htc/saga/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
-    vendor/htc/saga/proprietary/lib/libv8.so:obj/lib/libv8.so \
-    vendor/htc/saga/proprietary/lib/libjni_latinime.so:obj/lib/libjni_latinime.so
 
 # All the blobs necessary for saga
 PRODUCT_COPY_FILES += \
-    vendor/htc/saga/proprietary/bin/akmd:/system/bin/akmd \
     vendor/htc/saga/proprietary/bin/awb_camera:/system/bin/awb_camera \
-    vendor/htc/saga/proprietary/bin/bma150_usr:/system/bin/bma150_usr \
     vendor/htc/saga/proprietary/bin/lsc_camera:/system/bin/lsc_camera \
-    vendor/htc/saga/proprietary/bin/rmt_storage:/system/bin/rmt_storage \
     vendor/htc/saga/proprietary/vendor/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
     vendor/htc/saga/proprietary/etc/firmware/default.acdb:system/etc/firmware/default.acdb \
     vendor/htc/saga/proprietary/etc/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
@@ -40,36 +34,5 @@ PRODUCT_COPY_FILES += \
     vendor/htc/saga/proprietary/etc/firmware/vidc_720p_mp4_dec_mc.fw:system/etc/firmware/vidc_720p_mp4_dec_mc.fw \
     vendor/htc/saga/proprietary/etc/firmware/vidc_720p_mp4_enc_mc.fw:system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
     vendor/htc/saga/proprietary/etc/firmware/vidc_720p_vc1_dec_mc.fw:system/etc/firmware/vidc_720p_vc1_dec_mc.fw \
-    vendor/htc/saga/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
-    vendor/htc/saga/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
-    vendor/htc/saga/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    vendor/htc/saga/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/htc/saga/proprietary/lib/egl/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
-    vendor/htc/saga/proprietary/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
-    vendor/htc/saga/proprietary/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/htc/saga/proprietary/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/saga/proprietary/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/saga/proprietary/lib/egl/libGLESv2S3D_adreno200.so:/system/lib/egl/libGLESv2S3D_adreno200.so \
-    vendor/htc/saga/proprietary/lib/libaudioalsa.so:/system/lib/libaudioalsa.so \
-    vendor/htc/saga/proprietary/lib/libC2D2.so:/system/lib/libC2D2.so \
-    vendor/htc/saga/proprietary/lib/libc2d2_z180.so:/system/lib/libc2d2_z180.so \
     vendor/htc/saga/proprietary/lib/libcamera.so:/system/lib/libcamera.so \
-    vendor/htc/saga/proprietary/lib/liboemcamera.so:/system/lib/liboemcamera.so \
-    vendor/htc/saga/proprietary/lib/hw/camera.default.so:/system/lib/hw/camera.default.so \
-    vendor/htc/saga/proprietary/lib/libOlaEngine.so:/system/lib/libOlaEngine.so \
-    vendor/htc/saga/proprietary/lib/libcameraface.so:/system/lib/libcameraface.so \
-    vendor/htc/saga/proprietary/lib/libcamerapp.so:/system/lib/libcamerapp.so \
-    vendor/htc/saga/proprietary/lib/libchromatix_s5k4e1gx_default_video.so:/system/lib/libchromatix_s5k4e1gx_default_video.so \
-    vendor/htc/saga/proprietary/lib/libchromatix_s5k4e1gx_preview.so:/system/lib/libchromatix_s5k4e1gx_preview.so \
-    vendor/htc/saga/proprietary/lib/libgemini.so:/system/lib/libgemini.so \
-    vendor/htc/saga/proprietary/lib/libgsl.so:/system/lib/libgsl.so \
-    vendor/htc/saga/proprietary/lib/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
-    vendor/htc/saga/proprietary/lib/libhtc_ril.so:/system/lib/libhtc_ril.so \
-    vendor/htc/saga/proprietary/lib/libmmipl.so:/system/lib/libmmipl.so \
-    vendor/htc/saga/proprietary/lib/libmmjpeg.so:/system/lib/libmmjpeg.so \
-    vendor/htc/saga/proprietary/lib/liboemcamera.so:/system/lib/liboemcamera.so \
-    vendor/htc/saga/proprietary/lib/libOpenVG.so:/system/lib/libOpenVG.so \
     vendor/htc/saga/proprietary/lib/libposteffect.so:/system/lib/libposteffect.so \
-    vendor/htc/saga/proprietary/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
-    vendor/htc/saga/proprietary/lib/libv8.so:system/lib/libv8.so \
-    vendor/htc/saga/proprietary/lib/libjni_latinime.so:system/lib/libjni_latinime.so
